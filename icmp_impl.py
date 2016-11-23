8b687f68d794b21e794055baa220087fedffb473
zq9120 <zq9120@yeah.net>
2016-11-23 16:37:28 +0800

    update

--- icmp_impl.py
+++ icmp_impl.py
@@ -1,6 +1,14 @@
 import os
+from MySQL import *
+db = MySQL()
 
-total_count = 20
-for i in range(0, total_count):
-    print i, "of", total_count
-    os.system("python icmp.py > /dev/null 2>&1")
+# total_count = 20
+# for i in range(0, total_count):
+#     print (i + 1), "of", total_count
+#     os.system("python icmp.py > /dev/null 2>&1")
+
+sql = "SELECT * FROM kw_icmp"
+rel = db.query(sql)
+
+for v in rel:
+    print v
