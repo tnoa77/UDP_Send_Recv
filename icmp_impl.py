49340eda5298b3ad31f37269f6014ffbf1f524a8
zq9120 <zq9120@yeah.net>
2016-11-23 16:34:36 +0800

    update

--- icmp_impl.py
+++ icmp_impl.py
@@ -1,4 +1,6 @@
 import os
 
-while True:
-    os.system("python icmp.py")
+total_count = 20
+for i in range(0, total_count):
+    print i, "of", total_count
+    os.system("python icmp.py > /dev/null 2>&1")
